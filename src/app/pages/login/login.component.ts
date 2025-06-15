f6d3d308d4b56ebcf9bc3afcee9443bf99c05182
jabrawand <jasmin_brawand@hotmail.com>
2025-06-15 11:10:37 +0200

    routing to Dashboard works

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,35 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validator, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [
-    
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
-  onLogin() {
-    this.router.navigate(['/dashboard']);
+  loginForm: FormGroup;
+  errorMessage: string | null = null;
+
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
+  onLogin() {
+    const {username, password } = this.loginForm.value;
+    if (this.authService.login(username, password)) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.errorMessage = 'Ungültiger Benutzername oder Passwort';
+    }
+  }
+
 
 }
