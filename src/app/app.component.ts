ecff14d72f986e71fb46f8b093aa7984889c74c8
jabrawand <jasmin_brawand@hotmail.com>
2025-06-15 09:49:38 +0200

    added login container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -12,8 +16,5 @@ export class AppComponent {
   title = 'whiteboard-app';
   currentDate: string = new Date().toLocaleDateString();
  
-  @Output() login = new EventEmitter<void>();
- Login() {
-  this.login.emit();
- }
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Router } from 'express';
 import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    {path: "", component: LoginComponent}
+    {path: "", component: LoginComponent},
+    {path: "login", component: LoginComponent},
+    {path: "dashboard", component: DashboardComponent}
 ];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
